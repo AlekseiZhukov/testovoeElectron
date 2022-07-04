fa5d652fc4d2b23d399c2909f97d391ea7cbcd61
AlekseiZhukov <alex__zhukov@mail.ru>
2022-07-04 21:24:11 +0300

    added saving files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import React, {useEffect, useRef, useState} from "react";
 import './App.css';
 
+const electron = window.require('electron');
+const ipcRenderer = electron.ipcRenderer;
+
+
 function App() {
+
+    //const [downloadProgress, setDownloadProgress] = useState(0)
+    const [downloadFiles, setDownloadFiles] = useState([])
+
+    const [value, setValue] = useState('')
+
+    const handleClick =  () => {
+        console.log('handleClick', value)
+        setValue('')
+        ipcRenderer.send('download', {
+            payload: {
+                url: value,
+            }
+        });
+
+    }
+
+    useEffect(() => {
+
+        ipcRenderer.on('download-complete', (event, args) => {
+            const newFile = {
+                fileName: args.fileName,
+                fileSize: args.fileSize,
+                path: args.path
+            };
+
+            setDownloadFiles( prevState => {
+                const newState = prevState;
+                if (prevState.find(file => file.fileName === newFile.fileName)) {
+                    return newState
+                }
+
+                if (prevState.length >= 5) {
+                    newState.shift();
+                    newState.push(newFile)
+                } else {
+                    newState.push(newFile)
+                }
+                console.log('setDownloadFiles', newState)
+                return newState
+            });
+
+        });
+
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
+        <button onClick={handleClick}>get file</button>
+        <ul>
+            {downloadFiles.map(file => (
+                <li key={file.fileName}><a href={'#'}>{file.fileName}</a></li>
+                )
+            )}
+        </ul>
+
+        <p>{value}</p>
+
     </div>
   );
 }
