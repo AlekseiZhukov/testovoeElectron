746e1171f7573f0a7470d41b1f93bee226d2ff66
AlekseiZhukov <alex__zhukov@mail.ru>
2022-07-06 21:48:41 +0300

    finished

--- electron/main.js
+++ electron/main.js
@@ -1,26 +1,30 @@
-const { app, BrowserWindow, ipcMain, dialog } = require('electron');
-const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
+const { app, BrowserWindow, ipcMain, dialog, shell } = require('electron');
+//const { default: installExtension, REACT_DEVELOPER_TOOLS } = require('electron-devtools-installer');
 const { download } = require('electron-dl');
 
-let mainWindow
+let mainWindow;
 
 function createWindow () {
     // Create the browser window.
     mainWindow = new BrowserWindow({
         width: 800,
         height: 600,
+        title:"download files",
+        //frame: false,
         webPreferences: {
             nodeIntegration: true,
             enableRemoteModule:true,
-            contextIsolation: false
+            contextIsolation: false,
+
         }
-    })
+    });
+    mainWindow.setMenu(null);
 
     //load the index.html from a url
-    mainWindow.loadURL('http://localhost:3000');
+    mainWindow.loadURL('http://localhost:3000/');
 
     // Open the DevTools.
-    mainWindow.webContents.openDevTools()
+    //mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
@@ -28,68 +32,55 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
     createWindow();
-    installExtension(REACT_DEVELOPER_TOOLS)
+    //installExtension(REACT_DEVELOPER_TOOLS);
 
-})
-
-
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        app.quit()
+        app.quit();
     }
-})
+});
 
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
 
     if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow()
+        createWindow();
     }
-})
+});
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
 
 ipcMain.on('download', async (event, {payload}) => {
     if (payload.url) {
-        let properties = payload.properties ? {...payload.properties} : {};
-
-        const defaultPath = app.getPath(properties.directory ? properties.directory : 'downloads');
-
-        const defaultFileName = properties.fileName ? properties.fileName : payload.url.split('/').pop();
-
-        let customURL = dialog.showSaveDialogSync({
+        const defaultPath = app.getPath( 'downloads');
+        const defaultFileName = payload.url.split('/').pop();
+        let customURL = dialog.showSaveDialogSync(mainWindow,{
             defaultPath: `${defaultPath}/${defaultFileName}`
         })
+
         if (customURL) {
             let filePath = customURL.split('\\');
-
             let filename = `${filePath.pop()}`;
-
             let directory = filePath.join('/');
-            console.log('ipcMain.on(download) directory', directory)
-            properties = {...properties, directory, filename};
+            const properties = {directory, filename};
 
             await download(BrowserWindow.getFocusedWindow(), payload.url, {
                 ...properties,
-                //saveAs: true,
-                onProgress: progress => {
-                    mainWindow.webContents.send('download-progress', progress)
-                },
                 onCompleted: item => {
                     mainWindow.webContents.send('download-complete', item)
                 }
             })
         }
     }
+})
 
-
-
-    //app.webContents.downloadURL(payload.url)
-
+ipcMain.on('openFile', (event, {path}) => {
+    shell.showItemInFolder(path)
 })
--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef, useState} from "react";
+import React, {useEffect, useState} from "react";
 import './App.css';
 
 const electron = window.require('electron');
@@ -7,63 +7,89 @@ const ipcRenderer = electron.ipcRenderer;
 
 function App() {
 
-    //const [downloadProgress, setDownloadProgress] = useState(0)
     const [downloadFiles, setDownloadFiles] = useState([])
-
     const [value, setValue] = useState('')
+    const [error, setError] = useState(null)
 
     const handleClick =  () => {
-        console.log('handleClick', value)
-        setValue('')
-        ipcRenderer.send('download', {
-            payload: {
-                url: value,
-            }
-        });
+        const validUrl = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/
 
+        console.log('validUrl.test(value)', validUrl.test(value))
+        if (validUrl.test(value)) {
+            ipcRenderer.send('download', {
+                    payload: {
+                        url: value,
+                    }
+                });
+        } else {
+            setError("invalid value on url")
+        }
+    }
+
+    const handleChange = (e) => {
+        setError(null)
+        setValue(e.target.value)
+    }
+
+    const handleClickLink = (path) => {
+        ipcRenderer.send('openFile', {path});
+    }
+
+    const updateStateDownloadFiles = (newFile) => {
+        setDownloadFiles( prevState => {
+            const newState = prevState;
+            if (prevState.find(file => file.fileName === newFile.fileName)) {
+                return newState
+            }
+            if (prevState.length >= 5) {
+                newState.shift();
+                newState.push(newFile)
+            } else {
+                newState.push(newFile)
+            }
+            return newState
+        });
     }
 
     useEffect(() => {
 
-        ipcRenderer.on('download-complete', (event, args) => {
+        ipcRenderer.on('download-complete', (event, item) => {
             const newFile = {
-                fileName: args.fileName,
-                fileSize: args.fileSize,
-                path: args.path
+                fileName: item.path.split('\\').pop(),
+                fileSize: item.fileSize,
+                path: item.path
             };
 
-            setDownloadFiles( prevState => {
-                const newState = prevState;
-                if (prevState.find(file => file.fileName === newFile.fileName)) {
-                    return newState
-                }
-
-                if (prevState.length >= 5) {
-                    newState.shift();
-                    newState.push(newFile)
-                } else {
-                    newState.push(newFile)
-                }
-                console.log('setDownloadFiles', newState)
-                return newState
-            });
-
+            updateStateDownloadFiles(newFile)
+            setValue('')
         });
+
 
     }, [])
 
+
   return (
-    <div>
-        <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
-        <button onClick={handleClick}>get file</button>
-        <ul>
+    <div className="root">
+        <label  className={error && "hidden"}>
+            enter url address:
+        <input
+            type="text"
+            value={value}
+            onChange={handleChange}
+            placeholder="http://..."
+        />
+
+        </label>
+        {error && <p className="error" >{error}</p>}
+        <button disabled={!value.length>0} onClick={handleClick}>get file</button>
+        <ol>
             {downloadFiles.map(file => (
-                <li key={file.fileName}><a href={'#'}>{file.fileName}</a></li>
+                <li key={file.fileName}><button onClick={() => handleClickLink(file.path)}>{file.fileName}</button></li>
                 )
             )}
-        </ul>
+        </ol>
 
-        <p>{value}</p>
+
 
     </div>
   );
